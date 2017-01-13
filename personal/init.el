aef46f40b74f17950a7ea00799a88a98dcaed023
Andrew Morrow <acm@mongodb.com>
2017-01-13 10:56:48 -0500

    personalization

---
+++ personal/init.el
@@ -0,0 +1,29 @@
+(add-to-list 'load-path "~/Documents/10gen/dev/kernel-tools/codereview")
+
+(require 'vc-hg)
+(require 'vc-git)
+(require 'vc-svn)
+(require 'git-gutter-fringe)
+(require 'cmake-mode)
+(require 'markdown-mode)
+(require 'clang-format)
+(require 'xgen-cru)
+
+(load-theme 'solarized-dark t)
+
+;;(setq mac-command-modifier 'meta)
+;;(setq mac-option-modifier 'super)
+
+(global-linum-mode t)
+
+;; (setq prelude-clean-whitespace-on-save nil)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flyspell-mode)
+
+;; (setq whitespace-line-column 100) ;; limit line length
+
+;; (setq uniquify-buffer-name-style 'post-forward)
+
+(server-start)
+
